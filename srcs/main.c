8288aac15a4b217cf2688721a5e8ea4d8700c239
Lauri Litovuo <llitovuo@student.hive.fi>
2024-04-17 12:40:03 +0300

    removing exec files and updating main

--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aneitenb <aneitenb@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: llitovuo <llitovuo@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 10:22:38 by llitovuo          #+#    #+#             */
-/*   Updated: 2024/04/16 16:49:10 by aneitenb         ###   ########.fr       */
+/*   Updated: 2024/04/17 12:18:10 by llitovuo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int	main()		//this is just to check that Makefile and libft work
 {
 	char	*str = NULL;
-	int		res;		
+	int		res;
 	
 	res = ft_strlen(str);
 	ft_printf("%d\n", res);
