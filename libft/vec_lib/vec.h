bafb385f9a31438b681660561d3a80b2368c1609
Lauri Litovuo <llitovuo@student.hive.fi>
2024-04-19 15:35:59 +0300

    adding few prototypes

--- libft/vec_lib/vec.h
+++ libft/vec_lib/vec.h
@@ -6,7 +6,7 @@
 /*   By: llitovuo <llitovuo@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 13:05:28 by llitovuo          #+#    #+#             */
-/*   Updated: 2024/04/19 14:01:27 by llitovuo         ###   ########.fr       */
+/*   Updated: 2024/04/19 15:35:36 by llitovuo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,20 +33,15 @@ int		vec_new(t_vec *dst, size_t init_len, size_t elem_size);
 void	vec_free(t_vec *src);
 int		vec_from(t_vec *dst, void *src, size_t len, size_t elem_size);
 int		vec_resize(t_vec *src, size_t target_len);
-// int		vec_clear(t_vec *src);
+int		vec_clear(t_vec *src);
 int		vec_push(t_vec *src, void *elem);
 int		vec_pop(void *dst, t_vec *src);
 int		vec_copy(t_vec *dst, t_vec *src);
 void	*vec_get(t_vec *src, size_t index);
-// int		vec_insert(t_vec *dst, void *elem, size_t index);
+int		vec_insert(t_vec *dst, void *elem, size_t index);
 int		vec_remove(t_vec *src, size_t index);
 int		vec_append(t_vec *dst, t_vec *src);
-// int		vec_prepend(t_vec *dst, t_vec *src);
-// void	vec_iter(t_vec *src, void (*f) (void *));
-// void	*vec_find(t_vec *src, bool (*f) (void *));
-// int		vec_map(t_vec *dst, t_vec *src, void (*f) (void *));
-// int		vec_filter(t_vec *dst, t_vec *src, bool (*f) (void *));
-// int		vec_reduce(void *dst, t_vec *src, void (*f) (void *, void *));
-// void	vec_sort(t_vec *src, int (*f)(void *, void *));
+int		vec_prepend(t_vec *dst, t_vec *src);
+void	vec_iter(t_vec *src, void (*f) (void *));
 
 #endif
