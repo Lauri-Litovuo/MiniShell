1c583e3f2399777f44923474fa848f36c6ad0ae8
Aida Neitenbach <aneitenb@c1r3p8.hive.fi>
2024-04-24 14:35:31 +0300

    adding first go of splitting command line by pipe, still buggy and leaky

---
+++ srcs/parsing/parse.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aneitenb <aneitenb@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/16 14:10:11 by aneitenb          #+#    #+#             */
+/*   Updated: 2024/04/24 14:23:49 by aneitenb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../incl/minishell.h"
+
+int	init_vectors(t_vec *larg, t_shell *sh, char *buf)
+{
+	while (buf[sh->i] && (buf[sh->i] == ' ' || buf[sh->i] == '\t' || buf[sh->i] == '\n'))
+		sh->i++;
+	if (buf[sh->i] == '|')
+		return (error_msg_free(2, SYNTX, "`|'\n", larg));
+	while (buf[sh->i])
+	{
+		if (buf[sh->i] == '|')
+			sh->i++;
+		while (buf[sh->i] && (buf[sh->i] == ' ' || buf[sh->i] == '\t' || buf[sh->i] == '\n'))
+			sh->i++;
+		sh->j = sh->i;
+		while (buf[sh->i] && buf[sh->i] != '|')
+			sh->i++;
+		sh->temp = ft_substr(buf, sh->j, sh->i - sh->j);
+		if (sh->temp == NULL)
+			return(error_msg_free(1, SUBSTR, NULL, larg));
+		if (vec_push(larg, &sh->temp) < 0)
+			return (error_msg_free(1, VECPUSH, NULL, larg));
+		sh->i++;
+	}
+	init_index(sh);
+	return (1);
+}
+
+int	parse_input(t_vec *larg, t_shell *sh, char *buf)
+{
+	// if (check_syntax(sh, buf) == -1)
+	// 	return(-1);
+	if (vec_new(larg, 50, sizeof(char *)) < 0)
+		return (error_msg(1, VECNEW, NULL));
+	if (init_vectors(larg, sh, &buf[sh->i]) == -1)
+		return (-1);
+	return (1);
+}
