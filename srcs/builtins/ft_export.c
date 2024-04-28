9167761cbb9b689339ffd6bcf43686cfe1d4252b
Lauri Litovuo <llitovuo@student.hive.fi>
2024-04-28 16:22:59 +0300

    making export to work with solely export input

--- srcs/builtins/ft_export.c
+++ srcs/builtins/ft_export.c
@@ -6,42 +6,34 @@
 /*   By: llitovuo <llitovuo@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 10:22:49 by llitovuo          #+#    #+#             */
-/*   Updated: 2024/04/24 16:40:01 by llitovuo         ###   ########.fr       */
+/*   Updated: 2024/04/28 16:02:01 by llitovuo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../incl/minishell.h"
 
 // Should be tested if export works otherwise than as a first argument in bash.
-// Should test whether "export" should work solely without arguments, if so original t_vec env should be saved somewhere
 // should be added to include += which appends the string to the end of the env_val
+//for some reason += is not working correctly but adds JEE+=SMTHN.
 static int	check_export_syntax(char *arg);
 static int	export_variable(t_vec *env, char *arg);
 static int	export_env_var(char *env_var, char *arg, t_vec *env);
 
 int	ft_export(t_vec *env, t_vec *args)
 {
-	int		i;
-	int		j;
-	char	**arg_strs;
+	size_t		i;
+	size_t		j;
+	char		**arg_strs;
 
 	i = 0;
 	j = 0;
 	arg_strs = (char **)args->memory;
 	if (args->len == 1)
 	{
-		while ((size_t)j < env->len)
-		{
-			if (vec_get(env, j) == NULL)
-			{
-				printf("error in export1\n"); //change to error mngmt
-				return (1);
-			}
-			printf("%s\n", *(char **)vec_get(env, j));
-			i++;
-		}
+		print_exports(env);
+		return (0);
 	}
-	while ((size_t)i < args->len)
+	while (i < args->len)
 	{
 		if (export_variable(env, arg_strs[++i]) < 0)
 			printf("error in export2\n"); //change to error mngmt
@@ -54,16 +46,14 @@ static int	export_variable(t_vec *env, char *arg)
 	char	*env_var;
 
 	env_var = NULL;
-	if (ft_strchr(arg, '=') != NULL)
+	if (ft_strchr(arg, '=') != NULL && arg[1] != '=')
 	{
 		env_var = extract_env_var(arg);
 		if (env_var == NULL)
 			return (-1);
 		if (check_export_syntax(env_var) < 0)
-		{
-			printf("bash: export: `L+OL=hello.world': not a valid identifier\n"); //exitcode 1;
-		}
-		else if (check_export_syntax(env_var) >= 0)
+			printf("bash: export: `L+OL=hello.world': not a valid identifier\n") ; //exitcode 1;
+		else
 		{
 			if (export_env_var(env_var, arg, env) < 0)
 				return (-1);
@@ -109,10 +99,8 @@ static int	export_env_var(char *env_var, char *arg, t_vec *env)
 		if (index < 0)
 			return (-1);
 		temp = ft_strdup(arg);
-		if (vec_insert(env, &temp, index) < 0)
-			return (printf("free_temp_and_error"), -1); //error mngmt
-		if (vec_remove(env, (size_t)index - 1) < 0)
-			return (printf("free temp and error"), -1); //error mngmnt
+		if (vec_replace_str(env, temp, index) < -1)
+			return (printf("err with vec_replace\n"), -1);
 	}
 	return (0);
 }
