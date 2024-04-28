0caf3631620cdf16001968b8cfcfd38d835ee838
Lauri Litovuo <llitovuo@student.hive.fi>
2024-04-28 16:23:38 +0300

    initializing the drafted cd builtin function

---
+++ srcs/builtins/cd_utils.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cd_utils.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: llitovuo <llitovuo@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/28 16:16:15 by llitovuo          #+#    #+#             */
+/*   Updated: 2024/04/28 16:19:25 by llitovuo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../incl/minishell.h"
+
+int	goto_home(t_vec *env, char *home)
+{
+	
+}
+
+int	goto_root(void)
+{
+	
+}
+
+int	goto_parent(t_vec *env)
+{
+	
+}
+
+int	goto_grandparent(t_vec *env)
+{
+	
+}
+
+int	goto_dir(char	*path)
+{
+	
+}
--- srcs/builtins/ft_cd.c
+++ srcs/builtins/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: llitovuo <llitovuo@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 11:27:56 by llitovuo          #+#    #+#             */
-/*   Updated: 2024/04/24 12:12:46 by llitovuo         ###   ########.fr       */
+/*   Updated: 2024/04/28 16:16:01 by llitovuo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,29 +16,49 @@ static int	update_pwd_env(t_vec *env);
 
 int	ft_cd(t_vec *env, t_vec *args)
 {
-	if (cd only or cd ~ go to home eg. /Users/llitovuo);
-	else if(cd / goes to PWD=/)
-	else if(.. go one level up, if not on root);
-	else if (../.. go two levels up etc. if possible)
-	else if (cd [dirname] goes to that path)
+	char	**strs;
+	char	*home;
+
+	strs = (char **)args->memory;
+	copy_homedir(env, home);
+	if (!strs[0][1] ||
+	ft_strncmp(strs[0][1], "~", ft_strlen(strs[0][1]) + 1) ||
+	ft_strncmp(strs[0][1], home, ft_strlen(strs[0][1]) + 1))
+		goto_home(vec, home);
+	else if (ft_strncmp(strs[0][1], "/", ft_strlen(strs[0][1]) + 1))
+		goto_root();
+	else if (ft_strncmp(strs[0][1], "..", ft_strlen(strs[0][1]) + 1))
+		goto_parent();
+	else if (ft_strncmp(strs[0][1], "../..", ft_strlen(strs[0][1]) + 1))
+		goto_grandparent();
+	else if (access(strs[0][1], F_OK) == 0)
+		goto_dir();
+	else
+		return (-1); //err_mngmt
+}
+
+char	*copy_homedir(t_vec *env, char *home)
+{
 	
 }
 
+
+
 static int	update_pwd_env(t_vec *env)
 {
-	char	cur_dir[PATH_MAX]; //check if path_max (1024) is enough or should we use 4096
 	char	*temp;
 	int		index;
+	char	cur_dir[PATH_MAX]; //check if path_max (1024) is enough or should we use 4096
 
 	if (getcwd(cur_dir, PATH_MAX) == NULL)
 		return (perror("pwd: "), -1); // err_mngmt
 	index = find_index_of_env(env, "PWD");
 	if (index < 0)
 		return (printf("error in update_pwd_env1"), -1);// err_mngmt
-	if (vec_remove(env, index) < 0)
-		return (printf("error in update_pwd_env2"), -1);// err_mngmt
 	temp = ft_strdup(cur_dir);
-	if (vec_push(env, temp) < 0)
+	if (temp == NULL)
 		return (printf("error in update_pwd_env2"), -1);// err_mngmt
+	vec_replace_str(env, temp, index);
+		return (printf("error in update_pwd_env4"), -1);// err_mngmt
 	return (0);
 }
