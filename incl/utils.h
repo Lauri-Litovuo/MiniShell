6e44fa50bf63845a7db3a319cbdc09b21a8c3573
Lauri Litovuo <llitovuo@student.hive.fi>
2024-05-13 10:28:02 +0300

    Extracted utils to their own headerfile

---
+++ incl/utils.h
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: llitovuo <llitovuo@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/13 09:28:32 by llitovuo          #+#    #+#             */
+/*   Updated: 2024/05/13 09:32:46 by llitovuo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef UTILS_H
+# define UTILS_H
+
+typedef struct s_expd
+{
+	char	*temp;
+	char	*str;
+	char	*expanded;
+	char	*env_var;
+	char	*new;
+	int		ret;
+	size_t	var_len;
+	size_t	exp_len;
+	size_t	pre_len;
+	size_t	total_len;
+	size_t	index;
+	size_t	ds;
+	size_t	i;
+	size_t	var_index;
+}	t_expd;
+
+extern int	g_exit_status;
+
+int		expand_variables(t_vec *env, t_shell *arg);
+int		expand_cmd_and_rdrct(t_vec *env, t_vec *vec);
+int		expand_string(t_vec *env, t_expd *s, t_vec *vec);
+int		expand_to_env_var(t_vec *env, t_expd *s, t_vec *vec);
+int		get_exit_status(t_expd *s, t_vec *vec);
+void	free_expd(t_expd *s);
+
+#endif
+
