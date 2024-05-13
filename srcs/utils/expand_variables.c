82c76c0eb1ceb769974e4d1f85ce647b0c3c5348
Lauri Litovuo <llitovuo@student.hive.fi>
2024-05-13 10:27:18 +0300

    Making a function for 0 and empty env_variable

--- srcs/utils/expand_variables.c
+++ srcs/utils/expand_variables.c
@@ -6,13 +6,18 @@
 /*   By: llitovuo <llitovuo@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 09:44:24 by llitovuo          #+#    #+#             */
-/*   Updated: 2024/05/12 16:20:01 by llitovuo         ###   ########.fr       */
+/*   Updated: 2024/05/13 10:23:26 by llitovuo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../incl/minishell.h"
 
-int	expand_variables(t_vec *env, t_shell *arg)
+int			expand_cmd_and_rdrct(t_vec *env, t_vec *vec);
+static void	init_expd_struct(t_expd *s);
+int			expand_string(t_vec *env, t_expd *s, t_vec *vec);
+static int	check_if_exists(t_vec *env, t_expd *s);
+
+/*int	expand_variables(t_vec *env, t_shell *arg)
 {
 	if (&arg->cmd.len != 0)
 	{
@@ -24,6 +29,17 @@ int	expand_variables(t_vec *env, t_shell *arg)
 		if (expand_cmd_and_rdrct(env, &arg->rdrct) < 0)
 			return (-1);
 	}
+	return (0);
+}*/
+
+int	expand_variables(t_vec *env, t_vec *vec)
+{
+	if (vec->len != 0)
+	{
+		if (expand_cmd_and_rdrct(env, vec) < 0)
+			return (-1);
+	}
+	return (0);
 }
 
 int	expand_cmd_and_rdrct(t_vec *env, t_vec *vec)
@@ -35,16 +51,17 @@ int	expand_cmd_and_rdrct(t_vec *env, t_vec *vec)
 	{
 		s.str = *(char **)vec_get(vec, s.index);
 		s.total_len = ft_strlen(s.str);
-		while (s.str[s.ds] != '\0')
+		while (s.str[s.ds + 1] != '\0')
 		{
 			if (s.str[s.ds] == '$')
+			{
 				s.pre_len = s.ds;
-			else if (s.str[s.ds] == '$' && s.str[s.ds + 1] == '$')
-				s.ret = get_pid(s.expanded);
-			else if (s.str[s.ds] == '$' && s.str[s.ds + 1] == '?')
-				s.ret = get_exit_status(s.expanded);
-			else if (s.str[s.ds] == '$')
-				s.ret = expand_string(env, &s, vec);
+				if (s.str[s.ds] == '$' && s.str[s.ds + 1] == '?')
+					s.ret = expand_to_exit_status(&s, vec);
+				else
+					s.ret = expand_string(env, &s, vec);
+				free_expd(&s);
+			}
 			if (s.ret < 0)
 				return (-1);
 			s.ds++;
@@ -60,6 +77,8 @@ static void	init_expd_struct(t_expd *s)
 	s->temp = NULL;
 	s->str = NULL;
 	s->expanded = NULL;
+	s->env_var = NULL;
+	s->new = NULL;
 	s->var_len = 0;
 	s->exp_len = 0;
 	s->i = 0;
@@ -69,17 +88,29 @@ static void	init_expd_struct(t_expd *s)
 
 int	expand_string(t_vec *env, t_expd *s, t_vec *vec)
 {
+	if (s->str[s->ds] == '\0' || s->str[s->ds + 1] == '$')
+		return (0);
 	s->ret = check_if_exists(env, &s);
-	if (s->ret > 0)
+	if (s->ret >= 0)
 	{
-		if (expand_to_env_var(env, s, vec) < 0)
-			return (-1);
+		if (s->ret > 0)
+		{
+			if (expand_to_env_var(env, s, vec) < 0)
+				return (-1);
+		}
+		if (s->ret == 0)
+		{
+			if (expand_to_empty(env, s, vec) < 0)
+				return (-1);
+		}
+		return (0);
 	}
 	else if (s->ret < 0)
 		return (-1);
+	return (0);
 }
 
-int	check_if_exists(t_vec *env, t_expd *s)
+static int	check_if_exists(t_vec *env, t_expd *s)
 {
 	s->i = s->ds + 1;
 	while (s->str[s->i] && \
--- srcs/utils/expand_variables_utils.c
+++ srcs/utils/expand_variables_utils.c
@@ -6,7 +6,7 @@
 /*   By: llitovuo <llitovuo@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/12 15:01:04 by llitovuo          #+#    #+#             */
-/*   Updated: 2024/05/12 16:23:04 by llitovuo         ###   ########.fr       */
+/*   Updated: 2024/05/13 10:14:43 by llitovuo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 int	expand_to_env_var(t_vec *env, t_expd *s, t_vec *vec)
 {
 	char	*dupped;
+
 	ft_strlcpy(s->expanded, \
 	(*(char **)vec_get(env, s->var_index) + s->var_len + 1), PATH_MAX);
 	s->exp_len = ft_strlen(s->expanded);
@@ -25,7 +26,7 @@ int	expand_to_env_var(t_vec *env, t_expd *s, t_vec *vec)
 	ft_strlcat(s->new, s->expanded, (s->total_len - s->var_len + s->exp_len));
 	if (!s->new)
 		return (-1);
-	ft_strlcat(s->new, s->str + s->ds + s->var_len, \
+	ft_strlcat(s->new, s->str + (s->ds + s->var_len), \
 	(s->total_len - s->var_len + s->exp_len));
 	if (!s->new)
 		return (-1);
@@ -36,14 +37,47 @@ int	expand_to_env_var(t_vec *env, t_expd *s, t_vec *vec)
 	return (0);
 }
 
-int	get_pid(char *expand)
+int	expand_to_empty(t_vec *env, t_expd *s, t_vec *vec)
 {
-	expand = ft_strdup("12312"); //arb value, make better
+	char	*dupped;
+
+	s->new = malloc((s->total_len - s->var_len) * 1);
+	s->new = ft_substr(s->str, 0, s->pre_len);
+	ft_strlcat(s->new, s->str + (s->ds + s->var_len), \
+	(s->total_len - s->var_len + s->exp_len));
+	if (!s->new)
+		return (-1);
+	dupped = ft_strdup(s->new);
+	if (!dupped)
+		return (-1);
+	vec_replace_str(vec, dupped, s->index);
 	return (0);
 }
 
+int	get_exit_status(t_expd *s, t_vec *vec)
+{
+	char	*dupped;
 
-int	get_exit_status(s.expanded);
+	s->exp_len = ft_strlen(s->expanded);
+	s->expanded = ft_itoa(g_exit_status);
+	s->exp_len = ft_strlen(s->expanded);
+	s->new = malloc((s->total_len + s->exp_len - 2) * 1);
+	s->new = ft_substr(s->str, 0, s->pre_len);
+	if (!s->new)
+		return (-1);
+	ft_strlcat(s->new, s->expanded, (s->total_len + s->exp_len - 2));
+	if (!s->new)
+		return (-1);
+	ft_strlcat(s->new, s->str + (s->ds + 2), \
+	(s->total_len + s->exp_len - 2));
+	if (!s->new)
+		return (-1);
+	dupped = ft_strdup(s->new);
+	if (!dupped)
+		return (-1);
+	vec_replace_str(vec, dupped, s->index);
+	return (0);
+}
 
 void	free_expd(t_expd *s)
 {
@@ -54,8 +88,17 @@ void	free_expd(t_expd *s)
 	if (s->expanded)
 		free (s->expanded);
 	if (s->env_var)
-		fre (s->env_var);
+		free (s->env_var);
 	if (s->new)
 		free (s->new);
+	s->var_len = 0;
+	s->exp_len = 0;
+	s->i = 0;
+	s->ret = 0;
+	s->temp = NULL;
+	s->str = NULL;
+	s->expanded = NULL;
+	s->env_var = NULL;
+	s->new = NULL;
 }
 
