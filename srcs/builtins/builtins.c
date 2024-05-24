469205574cc5b448750f31a36b566546a6d9a1c1
Lauri Litovuo <llitovuo@student.hive.fi>
2024-05-24 17:01:46 +0300

    Changes to make it work with struct array

--- srcs/builtins/builtins.c
+++ srcs/builtins/builtins.c
@@ -6,33 +6,32 @@
 /*   By: llitovuo <llitovuo@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 13:31:40 by llitovuo          #+#    #+#             */
-/*   Updated: 2024/05/24 12:28:43 by llitovuo         ###   ########.fr       */
+/*   Updated: 2024/05/24 16:58:12 by llitovuo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../incl/minishell.h"
 
-int	launch_builtin(t_vec *env, t_vec *cmd, t_redir *redir)
+int	launch_builtin(t_vec *env, t_exec *exe)
 {
-	char	**arg_strs;
 
-	(void)redir;
 	if (!cmd->memory)
 		return (1);
-	arg_strs = (char **)cmd->memory;
-	if (ft_strncmp(arg_strs[0], "env", ft_strlen(arg_strs[0]) + 1) == 0)
+	if (ft_strncmp(exe->cmd, "env", ft_strlen(exe->cmd) + 1) == 0)
 		ft_env(env, cmd);
-	else if (ft_strncmp(arg_strs[0], "pwd", ft_strlen(arg_strs[0]) + 1) == 0)
+	else if (ft_strncmp(exe->cmd, "pwd", ft_strlen(exe->cmd) + 1) == 0)
 		ft_pwd(env);
-	else if (ft_strncmp(arg_strs[0], "unset", ft_strlen(arg_strs[0]) + 1) == 0)
+	else if (ft_strncmp(exe->cmd, "unset", ft_strlen(exe->cmd) + 1) == 0)
 		ft_unset(env, cmd);
-	else if (ft_strncmp(arg_strs[0], "export", ft_strlen(arg_strs[0]) + 1) == 0)
+	else if (ft_strncmp(exe->cmd, "export", ft_strlen(exe->cmd) + 1) == 0)
 		ft_export(env, cmd);
-	else if (ft_strncmp(arg_strs[0], "echo", ft_strlen(arg_strs[0]) + 1) == 0)
+	else if (ft_strncmp(exe->cmd, "echo", ft_strlen(exe->cmd) + 1) == 0)
 		ft_echo(cmd);
-	else if (ft_strncmp(arg_strs[0], "cd", ft_strlen(arg_strs[0]) + 1) == 0)
+	else if (ft_strncmp(exe->cmd, "cd", ft_strlen(exe->cmd) + 1) == 0)
 		ft_cd(env, cmd);
 	// else if (ft_strncmp(args, "exit", ft_strlen(args)) == 0)
 	// 	ft_exit();
+	else
+		return (42);
 	return (0);
 }
