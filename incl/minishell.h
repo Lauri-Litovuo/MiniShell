b824ab2d8fcced8fe7243999b46e1d01a70a146d
Lauri Litovuo <llitovuo@student.hive.fi>
2024-05-27 13:42:44 +0300

    moving structs to their own header file

--- incl/minishell.h
+++ incl/minishell.h
@@ -6,14 +6,12 @@
 /*   By: llitovuo <llitovuo@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 10:22:10 by llitovuo          #+#    #+#             */
-/*   Updated: 2024/05/24 13:52:14 by llitovuo         ###   ########.fr       */
+/*   Updated: 2024/05/27 12:29:06 by llitovuo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
-
-# include "../libft/includes/libft.h"
 # define PATH_MAX 4096
 # define INT_MIN -2147483647
 
@@ -83,6 +81,11 @@
 # include <curses.h>
 
 # include <limits.h>
+# include "../libft/includes/libft.h"
+# include "structs.h"
+# include "utils.h"
+# include "execute.h"
+# include "builtins.h"
 
 /****************************
 *			MACROS			*
@@ -98,76 +101,6 @@
 /************************************
 *			SHELL STRUCT			*
 *************************************/
-
-typedef struct s_redir
-{
-	int		fd_in;
-	int		fd_out;
-	int		orig_fdin;
-	int		orig_fdout;
-	char	*infile;
-	char	*outfile;
-	char	*hd_lim;
-	char	*hd_file;
-	int		pipe_out;
-	int		pipe_in;
-	int		file_in;
-	int		file_out;
-	int		hd_in;
-	int		hd_pos;
-	int		re_pos;
-	int		i;
-}	t_redir;
-
-typedef struct s_exec
-{
-	t_redir		redir;
-	char		**cmd_argv;
-	char		*cmd;
-	char		*path;
-	int			ret;
-	int			*pipe_fd;
-	size_t		pos;
-}				t_exec;
-
-typedef struct s_shell
-{
-	t_vec	cmd;
-	t_vec	rdrct;
-	t_vec	env;
-	char	*temp;
-	size_t	count;
-	size_t	pipe_count;
-	size_t	gl_count;
-	size_t	i;
-	int		j;
-	int		*pids;
-	t_exec	**exe;
- 	int		join_flag;
-	int		end_flag;
-	int		expand_flag;
-	int		joinrd_flag;
-	int		endrd_flag;
-	int		expandrd_flag;
-}	t_shell;
-
-
-
-# include "utils.h"
-# include "execute.h"
-# include "builtins.h"
-
-typedef struct s_vecjoin
-{
-	char	*base;
-	char	*add;
-	char	*fin;
-	char	*remainder;
-	size_t	base_len;
-	size_t	add_len;
-	size_t	fin_len;
-    int		index;
-}	t_vecjoin;
 
 int		parse_input(t_shell *arg, char *buf);
 int		error_msg(int flag, char *str, char *specifier);
