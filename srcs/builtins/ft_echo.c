988f670c673febb0f80ccdcc5cabff49dbfe3039
Lauri Litovuo <llitovuo@student.hive.fi>
2024-06-04 14:34:52 +0300

    adding quote check

--- srcs/builtins/ft_echo.c
+++ srcs/builtins/ft_echo.c
@@ -6,13 +6,15 @@
 /*   By: llitovuo <llitovuo@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 10:05:18 by llitovuo          #+#    #+#             */
-/*   Updated: 2024/06/03 16:18:16 by llitovuo         ###   ########.fr       */
+/*   Updated: 2024/06/04 14:32:53 by llitovuo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../incl/minishell.h"
 
 static int	print_strings(t_vec *args, size_t i, int op_n);
+static int	check_for_quotes(char *str);
+static int	write_without_quotes(char *str, int *check, int quote);
 
 int	ft_echo(t_vec *args)
 {
@@ -43,11 +45,20 @@ static int	print_strings(t_vec *args, size_t i, int op_n)
 {
 	char	**strs;
 	int		check;
+	int		quote;
 
+	quote = 0;
+	check = 0;
 	strs = (char **)args->memory;
 	while (i < args->len)
 	{
-		ft_putstr(strs[i], &check);
+		printf("trying to print: %s\n", strs[i]);
+		quote = check_for_quotes(strs[i]);
+		printf("quote: %d\n", quote);
+		if (quote == 0)
+			ft_putstr(strs[i], &check);
+		else
+			write_without_quotes(strs[i], &check, quote);
 		if (i < args->len - 1)
 			ft_putstr(" ", &check);
 		i++;
@@ -56,4 +67,52 @@ static int	print_strings(t_vec *args, size_t i, int op_n)
 		return (0);
 	ft_putstr("\n", &check);
 	return (0);
+}
+
+static int	check_for_quotes(char *str)
+{
+	int	count_single;
+	int	count_double;
+
+	count_single = 0;
+	count_double = 0;
+	while (*str)
+	{
+		if (*str == '\'')
+			count_single++;
+		if (*str == '\"')
+			count_double++;
+		str++;
+	}
+	if (count_single % 2 == 0)
+		return (1);
+	else if (count_double % 2 == 0)
+		return (2);
+	else
+		return (0);
+	return (0);
+}
+
+static int	write_without_quotes(char *str, int *check, int quote)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	if (!str)
+		return (ft_putstr("(null)", check));
+	while (str[i])
+	{
+		if (quote == 2 && str[i] == '\"')
+			i++;
+		else if (quote == 1 && str[i] == '\'')
+			i++;
+		else
+		{
+			count += ft_putchar(str[i], check);
+			i++;
+		}
+	}
+	return (count);
 }
