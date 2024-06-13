37c2b822b490a9d96b1899146fbec442eda3af3f
Aida Neitenbach <aneitenb@c2r2p2.hive.fi>
2024-06-13 18:22:33 +0300

    fixed empty expand leaks

--- srcs/utils/expand_variables_utils.c
+++ srcs/utils/expand_variables_utils.c
@@ -6,7 +6,7 @@
 /*   By: aneitenb <aneitenb@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/12 15:01:04 by llitovuo          #+#    #+#             */
-/*   Updated: 2024/06/13 18:02:19 by aneitenb         ###   ########.fr       */
+/*   Updated: 2024/06/13 18:18:49 by aneitenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,6 @@ int	expand_to_empty(t_expd *s, t_vec *vec, int index)
 {
 	char	*dupped;
 
-	s->new = malloc((s->total_len - s->var_len + 1) * 1);
 	s->new = ft_substr(s->str, 0, s->pre_len);
 	ft_strlcat(s->new, s->str + (s->ds + s->var_len), \
 	(s->total_len - s->var_len + s->exp_len));
@@ -48,12 +47,39 @@ int	expand_to_empty(t_expd *s, t_vec *vec, int index)
 		return (-1);
 	dupped = ft_strdup(s->new);
 	if (!dupped)
+	{
+		free(s->new);
 		return (-1);
+	}
 	free(s->new);
 	vec_replace_str(vec, dupped, index);
 	return (0);
 }
 
+// int	expand_to_exit_status(t_shell *arg, t_expd *s, t_vec *vec, int index)
+// {
+// 	char	*dupped;
+
+// 	s->expanded = ft_itoa(arg->exit_code);
+// 	if (s->expanded == NULL)
+// 		return (-1);
+// 	s->exp_len = ft_strlen(s->expanded);
+// 	s->new = malloc((s->total_len - 2 + s->exp_len) * 1);
+// 	if (!s->new)
+// 		return (-1);
+// 	s->new = ft_substr(s->str, 0, s->pre_len);
+// 	if (!s->new)
+// 		return (-1);
+// 	s->join = ft_strjoin(s->new, s->expanded);
+// 	free(s->new);
+// 	s->new = ft_substr(s->str, (s->ds + 2), ft_strlen(s->str));
+// 	free(s->expanded);
+// 	dupped = ft_strjoin(s->join, s->new);
+// 	free(s->new);
+// 	free(s->join);
+// 	vec_replace_str(vec, dupped, index);
+// 	return (0);
+// }
 int	expand_to_exit_status(t_shell *arg, t_expd *s, t_vec *vec, int index)
 {
 	char	*dupped;
