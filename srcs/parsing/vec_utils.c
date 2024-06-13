31591023101cb6e20f40d3a2df4f98ee704272ec
Aida Neitenbach <aneitenb@c2r2p2.hive.fi>
2024-06-13 20:55:54 +0300

    norm fixes

--- srcs/parsing/vec_utils.c
+++ srcs/parsing/vec_utils.c
@@ -6,7 +6,7 @@
 /*   By: aneitenb <aneitenb@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 13:37:58 by aneitenb          #+#    #+#             */
-/*   Updated: 2024/06/13 19:54:43 by aneitenb         ###   ########.fr       */
+/*   Updated: 2024/06/13 20:52:57 by aneitenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,20 @@ int	vec_replace_special(t_vec *dst, void *src, size_t index)
 		return (-1);
 	free(str);
 	return (1);
+}
+
+void	reset_flags(t_shell *arg)
+{
+	arg->join_flag = -1;
+	arg->end_flag = 0;
+	arg->joinrd_flag = -1;
+	arg->endrd_flag = 0;
+	arg->expand_flag = 0;
+}
+
+void	init_s(t_vecjoin *s)
+{
+	s->base = NULL;
+	s->fin = NULL;
+	s->index = 0;
 }
