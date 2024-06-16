d8872c08e4714319ea50c9a91a00797e05f03dde
Aida Neitenbach <aneitenb@c2r2p2.hive.fi>
2024-06-16 12:28:55 +0300

    fixed norm too long function

--- incl/utils.h
+++ incl/utils.h
@@ -6,7 +6,7 @@
 /*   By: aneitenb <aneitenb@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 09:28:32 by llitovuo          #+#    #+#             */
-/*   Updated: 2024/06/16 12:08:40 by aneitenb         ###   ########.fr       */
+/*   Updated: 2024/06/16 12:24:24 by aneitenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,6 @@ int		expand_to_env_var(t_shell *arg, t_expd *s, t_vec *vec, int index);
 int		expand_to_exit_status(t_shell *arg, t_expd *s, t_vec *vec, int index);
 int		expand_to_empty(t_expd *s, t_vec *vec, int index);
 void	get_count(t_vec *vec, int index, t_expd *s);
+void	handle_expand(t_shell *arg, t_expd *s, t_vec *vec, int index);
 
 #endif
